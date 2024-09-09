8ae0bb72d479947d3c0b1a8a5d34aed71c731df8
Med Amine bt <60607576+iifast2@users.noreply.github.com>
2024-09-09 19:07:24 +0100

    Commit - version update

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Users Limits",
-    "version": "17.0.0.0",
+    "version": "17.0.0.1",
     "author": "Bytelegion",
     "website": "http://www.bytelegions.com",
     "depends": ["base"],
