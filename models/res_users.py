214329e161cb573e8dce76e79d953c38a46d6b93
Med Amine bt <60607576+iifast2@users.noreply.github.com>
2024-09-09 18:50:02 +0100

    Commit - Version 17 Modifs

--- models/res_users.py
+++ models/res_users.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
-from odoo import tools, api, models, _, fields
+from odoo import api, models, fields
 from odoo.exceptions import UserError
 import logging
+from lxml import etree  # Import etree for XML parsing
 
 _logger = logging.getLogger(__name__)
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    @api.model
-    def create(self, vals):
-        users = self.env['res.users'].search([])
-        user = self.env['res.users'].sudo().search([('login', '=', 'admin')])
-
-        _logger.info("Number of existing users: %s", len(users))
-        _logger.info("Admin user limit: %s", user.set_users_limit)
-
-        user_limits = user.set_users_limit + 2
-
-        if user_limits <= len(users):
-            raise UserError(_("Maximum number of users created. Please contact your system administrator!"))
-
-        res = super(ResUsers, self).create(vals)
-        return res
-
     set_users_limit = fields.Integer(string='Set User Limit', tracking=True)
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(ResUsers, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        
+        # Add logging for debugging
+        _logger.info("User login: %s", self.env.user.login)
+        
+        if self.env.user.login != 'admin':
+            doc = etree.XML(res['arch'])
+            for node in doc.xpath("//field[@name='set_users_limit']"):
+                node.set('invisible', '1')  # Set field invisible if user is not 'admin'
+            res['arch'] = etree.tostring(doc, encoding='unicode')
+        
+        return res
