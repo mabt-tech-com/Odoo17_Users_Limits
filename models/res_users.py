98eb004985b4a9eaf08b2b987fddadaf92e80d55
Med Amine bt <60607576+iifast2@users.noreply.github.com>
2024-09-09 18:38:17 +0100

    Commit - Version 17 update

--- models/res_users.py
+++ models/res_users.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from odoo import tools, api, models, _, fields
 from odoo.exceptions import UserError
-from odoo.tools import config
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
@@ -11,9 +12,15 @@ class ResUsers(models.Model):
     def create(self, vals):
         users = self.env['res.users'].search([])
         user = self.env['res.users'].sudo().search([('login', '=', 'admin')])
+
+        _logger.info("Number of existing users: %s", len(users))
+        _logger.info("Admin user limit: %s", user.set_users_limit)
+
         user_limits = user.set_users_limit + 2
+
         if user_limits <= len(users):
-            raise UserError("Maximum number of users created. Please contact to your system administrator!")
+            raise UserError(_("Maximum number of users created. Please contact your system administrator!"))
+
         res = super(ResUsers, self).create(vals)
         return res
 
